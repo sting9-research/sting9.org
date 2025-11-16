d8e158b6e59198c89edcbba5ffc1908726e53650
Guillaume Moigneu <g@moigneu.com>
2025-11-16 11:17:26 -0600

    Refactor Vite configuration to enhance plugin integration
    - Replaced nitroV2Plugin() with nitro() for improved compatibility
    - Added devtools() to the plugins array for enhanced development experience
    - Cleaned up commented-out code for better readability

--- sting9.org/vite.config.ts
+++ sting9.org/vite.config.ts
@@ -1,13 +1,38 @@
+// import { defineConfig } from 'vite'
+// import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+// import viteReact from '@vitejs/plugin-react'
+// import viteTsConfigPaths from 'vite-tsconfig-paths'
+// import tailwindcss from '@tailwindcss/vite'
+// import { nitroV2Plugin } from '@tanstack/nitro-v2-vite-plugin'
+
+// const config = defineConfig({
+//   plugins: [
+//     nitroV2Plugin(),
+//     // this is the plugin that enables path aliases
+//     viteTsConfigPaths({
+//       projects: ['./tsconfig.json'],
+//     }),
+//     tailwindcss(),
+//     tanstackStart(),
+//     viteReact(),
+//   ],
+// })
+
+// export default config
+
+
 import { defineConfig } from 'vite'
+import { devtools } from '@tanstack/devtools-vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
-import { nitroV2Plugin } from '@tanstack/nitro-v2-vite-plugin'
+import { nitro } from 'nitro/vite'
 
 const config = defineConfig({
   plugins: [
-    nitroV2Plugin(),
+    devtools(),
+    nitro(),
     // this is the plugin that enables path aliases
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
